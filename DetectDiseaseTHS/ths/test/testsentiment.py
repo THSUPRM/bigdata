2911c46ed87053f103a8a23655c8d1091125aaea
CristianCGarzon <garzoncristianc@gmail.com>
2018-06-27 12:52:31 -0400

    sentences modified to work

--- DetectDiseaseTHS/ths/test/testsentiment.py
+++ DetectDiseaseTHS/ths/test/testsentiment.py
@@ -4,7 +4,7 @@ import sys
 def main(optimizer):
     # P  = ProcessTweetsGlove("data/cleantextlabels.csv","data/glove.6B.50d.txt")
     # P  = ProcessTweetsGloveOnePass("data/cleantextlabels2.csv","data/glove.6B.50d.txt")
-    P = ProcessTweetsGloveOnePassHyperParam("data/cleantextlabels4.csv", "data/glove.6B.50d.txt", optimizer)
+    P = ProcessTweetsGloveOnePassHyperParam("data/cleantextlabels5.csv", "data/glove.6B.50d.txt", optimizer)
     # P = ProcessTweetsGloveOnePass("data/cleantextlabels3.csv", "data/glove.6B.50d.txt")
 
     P.process("trained/model14.json", "trained/model14.h5")
