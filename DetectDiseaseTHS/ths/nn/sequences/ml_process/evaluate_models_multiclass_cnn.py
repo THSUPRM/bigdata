1f5eeb9856bf7475c165d3da40ec6fb027697aea
CristianCGarzon <garzoncristianc@gmail.com>
2018-09-28 00:33:47 -0400

    Modularization to CNN models

--- DetectDiseaseTHS/ths/nn/sequences/ml_process/evaluate_models_multiclass_cnn.py
+++ DetectDiseaseTHS/ths/nn/sequences/ml_process/evaluate_models_multiclass_cnn.py
@@ -34,7 +34,10 @@ class EvaluateModelsMulticlassCNN(TweetsProcessor):
         # for combination in itertools.islice(params, 720, 1080):  # -> 3th host
         # for combination in params:
             count_models += 1
-            self.nn = TweetSentimentInceptionV2_3x3(max_len, g)
+            # self.nn = TweetSentimentInceptionV2_3x3(max_len, g)
+            self.nn = TweetSentimentInceptionV2_5x5(max_len, g)
+            # self.nn = TweetSentimentInceptionV2_5x5_Multi(max_len, g)
+            # self.nn = TweetSentimentInceptionV2_3x3_Multi(max_len, g)
             file_name = str(self.route_files) + "/model" + str(combination).replace(" ", "") + ".txt"
             log = open(file_name, "a+")
             start_time_comb = datetime.now()
